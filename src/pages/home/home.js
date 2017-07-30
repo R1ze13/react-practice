4ddf9c455af4a1d8ce3ae44f5afd43747e70a442
R1ze13 <justdieplz13@gmail.com>
2017-07-31 02:22:52 +0300

    todo app with redux

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import Input from '../../components/ui/input/index.js'
+import React, { PropTypes } from 'react';
+import Input from '../../components/ui/input/index'
 import { bindAll } from 'lodash';
+import { connect } from 'react-redux';
+import { addTodo } from './actions';
 import './styles.less';
 
-export default class HomePage extends React.Component {
+class HomePage extends React.Component {
 
     static path = '/';
+    static propTypes = {
+    	home: PropTypes.object.isRequired,
+    	dispatch: PropTypes.func.isRequired
+    };
 
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			todoName: '',
-			todos: [
-				{
-					id: 1,
-					name: 'todo 1'
-				}
-			],
-			error: ''
+			todoName: ''
 		};
 
 		bindAll(this, ['renderTodos', 'inputOnChange', 'addTodo']);
@@ -29,29 +28,22 @@ export default class HomePage extends React.Component {
 	}
 
 	addTodo() {
-		if (this.state.todoName === '') {
-			this.setState({ error: 'Поле не должно быть пустым' });
-			return;
-		}
-
-		const id = this.state.todos[this.state.todos.length - 1].id + 1;
+		const { todos } = this.props.home;
+		const id = todos[todos.length - 1].id + 1;
 		const name = this.state.todoName;
-
-		const todos = this.state.todos;
-		todos.push({ id, name });
-
-		this.setState({ todos });
-		this.setState({ todoName: '', error: '' });
+		this.props.dispatch( addTodo(id, name) );
+		this.setState({ todoName: '' });
 	}
 
 	renderTodos(item, idx) {
 		return (
 			<li key={ idx }>{ item.name }</li>
-		)
+		);
 	}
 
     render() {
-		const { todoName, todos, error } = this.state;
+		const { todoName } = this.state;
+		const { todos, error } = this.props.home;
         return (
             <div className='row-fluid b-home'>
 				<div className='col-xs-12'>
@@ -59,7 +51,10 @@ export default class HomePage extends React.Component {
 						{ todos.map(this.renderTodos) }
 					</ul>
 					<div className='col-xs-4'>
-						<Input onChange={ this.inputOnChange } value={ todoName } error={ error } />
+						<Input 
+							onChange={ this.inputOnChange } 
+							value={ todoName } 
+							error={ error } />
 						<button className='btn btn-primary' onClick={ this.addTodo }>Add todo</button>
 					</div>
 				</div>
@@ -68,3 +63,11 @@ export default class HomePage extends React.Component {
     }
 
 }
+
+function mapStateToProps(state) {
+	return {
+		home: state.home
+	};
+}
+
+export default connect(mapStateToProps)(HomePage);
