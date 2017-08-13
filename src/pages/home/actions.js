415e2a01c28aab60f5da502790d9eb7744010a80
R1ze13 <justdieplz13@gmail.com>
2017-08-13 22:51:11 +0300

    добавлен функционал лукасов и удаленияэ

--- src/pages/home/actions.js
+++ src/pages/home/actions.js
@@ -1,4 +1,6 @@
 export const ADD_TODO = 'ADD_TODO';
+export const LIKE_TODO = 'LIKE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
 
 export function addTodo(id, name) {
 	let error = '';
@@ -9,4 +11,20 @@ export function addTodo(id, name) {
 		type: ADD_TODO,
 		id, name, error
 	};
+}
+
+export function likeTodo(todo) {
+	const liked = !todo.liked;
+
+	return {
+		type: LIKE_TODO,
+		todo, liked
+	};
+}
+
+export function deleteTodo(todo) {
+	return {
+		type: DELETE_TODO,
+		todo
+	}
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react';
-import Input from '../../components/ui/input/index'
+import Input from '../../components/ui/input/index';
 import { bindAll } from 'lodash';
 import { connect } from 'react-redux';
-import { addTodo } from './actions';
+import { addTodo, likeTodo, deleteTodo } from './actions';
+import classnames from 'classnames';
 import './styles.less';
 
 class HomePage extends React.Component {
@@ -36,9 +37,28 @@ class HomePage extends React.Component {
 	}
 
 	renderTodos(item, idx) {
+		const todoClasses = classnames('b-home-todo', {
+			'is-liked': item.liked
+		});
+		const btnClasses = classnames('btn b-home-btn-glyph', {
+			'active': item.liked
+		});
+
 		return (
-			<li key={ idx }>{ item.name }</li>
+			<li key={ idx }>
+                <span className={ todoClasses }>{ item.name }</span>
+                <button className='btn b-home-btn-glyph' onClick={ this.deleteTodo.bind(this, item) }><i className='glyphicon glyphicon-remove' /></button>
+                <button className={ btnClasses } onClick={ this.likeTodo.bind(this, item) }><i className='glyphicon glyphicon-heart' /></button>
+            </li>
 		);
+	}
+
+	deleteTodo(todo) {
+		this.props.dispatch( deleteTodo(todo) );
+	}
+
+	likeTodo(todo) {
+		this.props.dispatch( likeTodo(todo) );
 	}
 
     render() {
@@ -51,11 +71,11 @@ class HomePage extends React.Component {
 						{ todos.map(this.renderTodos) }
 					</ul>
 					<div className='col-xs-4'>
-						<Input 
-							onChange={ this.inputOnChange } 
-							value={ todoName } 
+						<Input
+							onChange={ this.inputOnChange }
+							value={ todoName }
 							error={ error } />
-						<button className='btn btn-primary' onClick={ this.addTodo }>Add todo</button>
+						<button className='btn btn-primary b-home-submit' onClick={ this.addTodo }>Add todo</button>
 					</div>
 				</div>
 			</div>
